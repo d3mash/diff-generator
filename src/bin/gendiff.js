880e1a6d1df808415ad5b1c1733ab21e2b29dd9c
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-04 12:56:07 +0300

    add gendiff bin

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .arguments('<firstConfig>, <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
+program.parse(process.argv);

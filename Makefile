126ac01ff9580264d68224f7ebf2411f39b66ed0
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-05 20:12:20 +0300

    edit test architecture

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ install:
 publish:
 	npm publish
 lint:
-	npm run eslint src/** src/bin/** 
+	npm run eslint src/** src/bin/**
 run:
-		npm run babel-node -- 'src/bin/gendiff.js' -h
+		npm run babel-node -- 'src/bin/gendiff.js' __tests__/__fixtures__/json/config1.json __tests__/__fixtures__/json/config2.json
 test:
 	npm test
--- __tests__/plain-test.js
+++ __tests__/plain-test.js
@@ -2,7 +2,12 @@ import fs from 'fs';
 import genDiff from '../src/plain';
 
 
-test('Test N1', () => {
+test('plain JSON', () => {
+  const expectedN1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/join/expected.json', 'utf8'));
+  const resultN1 = genDiff('./__tests__/__fixtures__/json/config1.json', './__tests__/__fixtures__/json/config2.json');
+  expect(resultN1).toEqual(expectedN1);
+});
+test('plain YAML', () => {
   const expectedN1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/expected.json', 'utf8'));
   const resultN1 = genDiff('./__tests__/__fixtures__/config1.json', './__tests__/__fixtures__/config2.json');
   expect(resultN1).toEqual(expectedN1);

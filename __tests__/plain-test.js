0b5cef2106cf9eca28417c727318be419e538581
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-05 22:38:15 +0300

    add YAML support and tests

--- __tests__/plain-test.js
+++ __tests__/plain-test.js
@@ -3,14 +3,12 @@ import genDiff from '../src';
 
 
 test('plain JSON', () => {
-  const expectedN1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/json/expected.json', 'utf8'));
+  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
   const resultN1 = genDiff('./__tests__/__fixtures__/json/config1.json', './__tests__/__fixtures__/json/config2.json');
   expect(resultN1).toEqual(expectedN1);
 });
-// test('plain YAML', () => {
-//  const expectedN1 = JSON.parse(fs.readFileSync
-// ('./__tests__/__fixtures__/expected.json', 'utf8'));
-//  const resultN1 = genDiff('./__tests__/__fixtures__/config1.json',
-// './__tests__/__fixtures__/config2.json');
-//  expect(resultN1).toEqual(expectedN1);
-// });
+test('plain YAML', () => {
+  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
+  const resultN1 = genDiff('./__tests__/__fixtures__/yaml/config1.yml', './__tests__/__fixtures__/yaml/config2.yml');
+  expect(resultN1).toEqual(expectedN1);
+});
--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
+import { safeLoad } from 'js-yaml';
 import _ from 'lodash';
 import fs from 'fs';
+import path from 'path';
+
+const getParser = (format) => {
+  const parsers = {
+    json: JSON.parse,
+    yaml: safeLoad,
+    yml: safeLoad,
+  };
+  return (data) => {
+    const parse = parsers[format];
+    return parse(data);
+  };
+};
+const mapping = [
+  {
+    check: (old, updated, prop) => !_.has(old, prop), // added
+    setValue: (old, updated, prop) => `+ ${prop}: ${updated[prop]}`,
+  },
+  {
+    check: (old, updated, prop) => !_.has(updated, prop), // deleted
+    setValue: (old, updated, prop) => `- ${prop}: ${old[prop]}`,
+  },
+  {
+    check: (old, updated, prop) => old[prop] !== updated[prop], // modified
+    setValue: (old, updated, prop) => [`- ${prop}: ${old[prop]}`, `+ ${prop}: ${updated[prop]}`],
+  },
+  {
+    check: (old, updated, prop) => old[prop] === updated[prop], // unchanged
+    setValue: (old, updated, prop) => `${prop} : ${old[prop]}`,
+  },
+];
+
+const getContent = pathToFile => fs.readFileSync(pathToFile, 'utf-8');
 
 export default (file1, file2) => {
-  const parse = path => JSON.parse(fs.readFileSync(path, 'utf8'));
-  const configOne = parse(file1);
-  const configTwo = parse(file2);
-  const mapping = [
-    {
-      check: (old, updated, prop) => !_.has(old, prop), // added
-      setValue: (old, updated, prop) => [`+ ${prop}`, updated[prop]],
-    },
-    {
-      check: (old, updated, prop) => !_.has(updated, prop), // deleted
-      setValue: (old, updated, prop) => [`- ${prop}`, old[prop]],
-    },
-    {
-      check: (old, updated, prop) => old[prop] !== updated[prop], // modified
-      setValue: (old, updated, prop) => [`- ${prop}`, old[prop], `+ ${prop}`, updated[prop]],
-    },
-    {
-      check: (old, updated, prop) => old[prop] === updated[prop], // unchanged
-      setValue: (old, updated, prop) => [`${prop}`, old[prop]],
-    },
-  ];
-  const allKeys = Object.keys(configOne).concat(Object.keys(configTwo));
-  const output = {};
-  const difference = allKeys.reduce((acc, element) => {
-    const { setValue } = _.find(mapping, ({ check }) => check(configOne, configTwo, element));
-    const prop = setValue(configOne, configTwo, element);
-    return prop[2] === undefined ? { ...acc, [prop[0]]: prop[1] } :
-      { ...acc, [prop[0]]: prop[1], [prop[2]]: prop[3] };
-  }, output);
-  return difference;
+  const format = path.extname(file1).slice(1);
+  const parse = getParser(format);
+  const configOne = parse(getContent(file1));
+  const configTwo = parse(getContent(file2));
+  const allKeys = _.union(Object.keys(configOne), Object.keys(configTwo));
+  const results = allKeys.map((key) => {
+    const { setValue } = _.find(mapping, ({ check }) => check(configOne, configTwo, key));
+    return setValue(configOne, configTwo, key);
+  });
+  return `{\n${_.flatten(results).join('\n')}\n}\n`;
 };

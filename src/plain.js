8be981ce50e7f954463668226258191f56448e79
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-05 13:34:17 +0300

    v 1.1.1, refactor plain

--- src/plain.js
+++ src/plain.js
@@ -1,32 +1,35 @@
-import { has } from 'lodash';
+import _ from 'lodash';
 import fs from 'fs';
 
 export default (file1, file2) => {
   const parse = path => JSON.parse(fs.readFileSync(path, 'utf8'));
   const configOne = parse(file1);
   const configTwo = parse(file2);
-  const mapping = {
-    add: (prop, old, updated) => [[`+ ${prop}`], updated[prop]],
-    del: (prop, old) => [[`- ${prop}`], old[prop]],
-    mod: (prop, old, updated) => [`- ${prop}`, `${old[prop]}`, `+ ${prop}`, `${updated[prop]}`],
-    unchanged: (prop, old) => [[`${prop}`], old[prop]],
-  };
-  const addedProps = Object.keys(configTwo).filter(element => !has(configOne, element))
-    .map(element => [element, 'add']);
-  const deletedProps = Object.keys(configOne).filter(element => !has(configTwo, element))
-    .map(element => [element, 'del']);
-  const modifiedProps = Object.keys(configTwo).filter(element => has(configOne, element)
-    && configTwo[element] !== configOne[element])
-    .map(element => [element, 'mod']);
-  const unchangedProps = Object.keys(configTwo).filter(element =>
-    has(configOne, element) && configTwo[element] === configOne[element])
-    .map(element => [element, 'unchanged']);
-  const allProps = addedProps.concat(deletedProps, modifiedProps, unchangedProps);
+  const mapping = [
+    {
+      check: (old, updated, prop) => !_.has(old, prop),
+      setValue: (old, updated, prop) => [`+ ${prop}`, updated[prop]],
+    },
+    {
+      check: (old, updated, prop) => !_.has(updated, prop),
+      setValue: (old, updated, prop) => [`- ${prop}`, old[prop]],
+    },
+    {
+      check: (old, updated, prop) => old[prop] !== updated[prop],
+      setValue: (old, updated, prop) => [`- ${prop}`, `${old[prop]}`, `+ ${prop}`, `${updated[prop]}`],
+    },
+    {
+      check: (old, updated, prop) => old[prop] === updated[prop],
+      setValue: (old, updated, prop) => [`${prop}`, old[prop]],
+    },
+  ];
+  const allKeys = Object.keys(configOne).concat(Object.keys(configTwo));
   const output = {};
-  const difference = allProps.reduce((acc, element) => {
-    const [prop, propType] = element;
-    const value = mapping[propType](prop, configOne, configTwo);
-    return propType !== 'mod' ? { ...acc, [value[0]]: value[1] } : { ...acc, [value[0]]: value[1], [value[2]]: value[3] };
+  const difference = allKeys.reduce((acc, element) => {
+    const { setValue } = _.find(mapping, ({ check }) => check(configOne, configTwo, element));
+    const prop = setValue(configOne, configTwo, element);
+    return prop[2] === undefined ? { ...acc, [prop[0]]: prop[1] } :
+      { ...acc, [prop[0]]: prop[1], [prop[2]]: prop[3] };
   }, output);
   return difference;
 };

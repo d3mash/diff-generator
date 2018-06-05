fa6898355147e73377a519a47ac3c3967ed5fa63
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-05 22:58:15 +0300

    fix tests duplicates

--- __tests__/plain-test.js
+++ __tests__/plain-test.js
@@ -3,12 +3,12 @@ import genDiff from '../src';
 
 
 test('plain JSON', () => {
-  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
-  const resultN1 = genDiff('./__tests__/__fixtures__/json/config1.json', './__tests__/__fixtures__/json/config2.json');
-  expect(resultN1).toEqual(expectedN1);
+  const expectedJSON = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
+  const resultJSON = genDiff('./__tests__/__fixtures__/json/config1.json', './__tests__/__fixtures__/json/config2.json');
+  expect(resultJSON).toEqual(expectedJSON);
 });
 test('plain YAML', () => {
-  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
-  const resultN1 = genDiff('./__tests__/__fixtures__/yaml/config1.yml', './__tests__/__fixtures__/yaml/config2.yml');
-  expect(resultN1).toEqual(expectedN1);
+  const expectedYAML = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf8');
+  const resultYAML = genDiff('./__tests__/__fixtures__/yaml/config1.yml', './__tests__/__fixtures__/yaml/config2.yml');
+  expect(resultYAML).toEqual(expectedYAML);
 });

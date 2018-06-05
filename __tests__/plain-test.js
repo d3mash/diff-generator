054af2e7786993cba71ee339e76d82ce8624ef9c
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-05 20:15:05 +0300

    fix typo

--- __tests__/plain-test.js
+++ __tests__/plain-test.js
@@ -3,7 +3,7 @@ import genDiff from '../src/plain';
 
 
 test('plain JSON', () => {
-  const expectedN1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/join/expected.json', 'utf8'));
+  const expectedN1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/json/expected.json', 'utf8'));
   const resultN1 = genDiff('./__tests__/__fixtures__/json/config1.json', './__tests__/__fixtures__/json/config2.json');
   expect(resultN1).toEqual(expectedN1);
 });
